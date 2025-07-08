8c9c2c6f841a1f1eec7c79ac3dea11d6956acde6
Diana Ramírez Gamboa <dianaramirezgamboa@outlook.com>
2025-07-08 17:43:08 -0600

    Update PFE.C2.E3.py
    Changed the code to avoid errors by the input of the user with a try and except statement

--- PFE.C2.E3.py
+++ PFE.C2.E3.py
@@ -1,7 +1,10 @@
 #Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay
 hours=input('Please enter the Hours: ')
 rate=input('Please enter the Rate: ')
-hours=float(hours)
-rate=float(rate)
-pay=hours*rate
-print('Pay: '+ str(pay))
+try:
+    hours=float(hours)
+    rate=float(rate)
+    pay = hours * rate
+    print(f"Pay: {str(pay)}")
+except (TypeError, ValueError) as e:
+    print("Please enter numeric values to avoid errors")
