9309bef80060600a2c7c9d3a9fb05d6cd926c613
Diana Ramírez Gamboa <dianaramirezgamboa@outlook.com>
2025-07-08 17:48:44 -0600

    Create PFE.C2.E5.py
    Finished the Exercise 5 making the conversion of temperature from Celsius to Fahrenheit

---
+++ PFE.C2.E5.py
@@ -0,0 +1,4 @@
+#Write a program which prompts the user for a Celsius temperature,convert the temperature to Fahrenheit, and print out the converted temperature.
+t_celsius=input('Please enter a temperature in °C: ')
+t_Fahrenheit=(float(t_celsius)*9/5)+32
+print(f"Temperature in °F: {t_Fahrenheit}")
