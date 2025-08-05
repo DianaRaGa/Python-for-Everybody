c2ed0c928973261ac34c4cb4a192b86e02283938
Diana Ramírez Gamboa <dianaramirezgamboa@outlook.com>
2025-08-05 09:24:19 -0600

    Create PFE.C13.1Coursera.py

---
+++ PFE.C13.1Coursera.py
@@ -0,0 +1,28 @@
+#In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/xml3.py.
+# The program will prompt for a URL, read the XML data from that URL using urllib and then parse and extract the comment
+# counts from the XML data, compute the sum of the numbers in the file.
+#web page to check the results with:
+#Sample data: http://py4e-data.dr-chuck.net/comments_42.xml (Sum=2553)
+#Actual data: http://py4e-data.dr-chuck.net/comments_2010035.xml (Sum ends with 97)
+
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = input('Enter the URL: ')
+if len(url) < 1 : 
+    url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print('Retrieved',len(data),'characters')
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+nums = list()
+for result in counts:
+    # Debug print the data :)
+    nums.append(int(result.text))
+
+print('Count:', len(nums))
+print('Sum:', sum(nums))
